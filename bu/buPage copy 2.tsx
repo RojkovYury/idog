c262b57cf84b67e7a37a503da5f717f566c4c9c6
RojkovYury <rojkov.yury@gmail.com>
2023-11-09 12:36:01 +0300

    lint 4 build

--- bu/buPage copy 2.tsx
+++ bu/buPage copy 2.tsx
@@ -1,3 +1,5 @@
+/*
+
 const waveBaseSXProps = { position: 'absolute', left: '0', width: '100%', height: '100px', background: `url('/images/wave.png')`, backgroundSize: '1000px 100px' }
 const buttonProps = {fontFamily: 'Amatic_SC', fontWeight: '700', backgroundColor: 'inherit', borderRadius: '25px', ml: 1, boxShadow: 'none', color: 'black', width: '125px', height: '40px', "&:hover": { backgroundColor: '#ff7c3f', color: 'white' }}
 const transitioProps = {  } //transition: 'top 0.5s linear'
@@ -23,7 +25,7 @@ useEffect(() => {
 }, []);
       
       
-      {/* page1 */}
+
       <Box sx={{ position: 'relative', background: '#F8F3F0', height: `${layout0H}px`, overflow: 'hidden', display: 'flex',  justifyContent: 'center', alignItems: 'center' }}>
 
         <Box sx={{ width: '500px', height: '350px', background: '#F8F3F0' }}></Box>
@@ -32,7 +34,7 @@ useEffect(() => {
         <Box sx={{ top: `${layout1Y}px`, ...transitioProps, position: 'absolute', zIndex: '15', width: '100%',  height: '100%', display: 'flex',  justifyContent: 'center', alignItems: 'center' }}>
           <Container maxWidth='lg' sx={{ position: 'relative', display: 'flex',  justifyContent: 'space-between', alignItems: 'flex-start' }}>
             
-            {/* слева */}
+
             <Box sx={{ position: 'relative', display: 'flex', flexDirection: 'column', pt: 4, height: '100%', width: '100%' }}>
 
               <Box sx={{ my: 10, height: '400px', display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
@@ -43,7 +45,7 @@ useEffect(() => {
               
             </Box>
 
-            {/* справа */}
+
             <Box sx={{ pt: 4, height: '100%',  display: 'flex', flexDirection: 'column', width: '100%' }}>
 
               <Box sx={{ width: "100%", height: "100%", mt: 4, display: "flex", flexDirection: "column", justifyContent: 'flex-start' }}>
@@ -55,9 +57,11 @@ useEffect(() => {
           </Container>
         </Box>
 
-        {/* середина */}
+
         <Box sx={{ top: `${layout2Y}px`, ...transitioProps, position: 'absolute', zIndex: '4', width: '100%',  height: '100%', display: 'flex',  justifyContent: 'center', alignItems: 'center' }}>
           <Box sx={{ m: 10, width: '300px', height: '300px', background: 'lightyellow' }}></Box>
         </Box>
 
       </Box>
+
+      */
